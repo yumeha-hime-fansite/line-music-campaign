4a09d5770788601a30a8763d286bcdadc71659b7
yumeha-hime-fansite <yumeha.hime.fan.calendar@gmail.com>
2026-08-23 10:55:55 +0900

    Make description optional in Reward type
    Updated Reward type to make description optional and modified individual rewards and milestones for clarity.

--- src/lib/data/campaign.ts
+++ src/lib/data/campaign.ts
@@ -1,7 +1,7 @@
 export type Reward = {
   plays?: number;
   title: string;
-  description: string;
+  description?: string;
 };
 
 export type Milestone = {
@@ -20,9 +20,9 @@ export const campaign = {
   songUrl: 'https://lin.ee/UrIsXn0',
   // 今回の再生キャンペーン公式告知ページ
   officialCampaignUrl: 'https://x.com/hime_yumeha/status/2090438524176851336?s=20',
-  //特典応募フォームのURL
+  // 特典応募フォームのURL
   applicationUrl: 'https://docs.google.com/forms/d/e/1FAIpQLScS5L5WI8LA5uWGCdiUnAlvffLf2WMAJXJfIKU3-uhq2ly1tg/viewform?usp=send_form',
-  //応募締切日
+  // 応募締切日
   applicationDeadline: '2026年10月3日',
 
   // ヒメさんから掲載許可が出た場合に static/images 配下へ置き、
@@ -32,26 +32,38 @@ export const campaign = {
 
   individualRewards: [
     {
-      title: '個人再生数プレゼント',
-      description: '500回再生スマホ用壁紙、1000回再生ジャケットデザインステッカー、5000回再生ジャケットデザイン缶バッジ、9999回再生コンプリート記念カード'
+      plays: 500,
+      title: 'スマホ用壁紙'
+    },
+    {
+      plays: 1_000,
+      title: 'ジャケットデザインステッカー'
+    },
+    {
+      plays: 5_000,
+      title: 'ジャケットデザイン缶バッジ'
+    },
+    {
+      plays: 9_999,
+      title: 'コンプリート記念カード'
     }
   ] satisfies Reward[],
 
   totalMilestones: [
     {
       plays: 500_000,
-      title: '50万再生達成',
-      description: '『mal d\'amour』『彩光ーイリザシオンー』カラオケ実装決定'
+      title: '2曲のカラオケ実装決定',
+      description: '「mal d\'amour」「彩光ーイリザシオンー」'
     },
     {
       plays: 700_000,
-      title: '70万再生達成',
-      description: 'オリジナル楽曲制作決定'
+      title: 'オリジナル楽曲制作決定',
+      description: 'みんなの再生が、新しい楽曲につながります。'
     },
     {
       plays: 1_000_000,
-      title: '100万再生達成',
-      description: 'オリジナル楽曲MV制作決定'
+      title: 'オリジナル楽曲MV制作決定',
+      description: '100万再生達成でMV制作が決定します。'
     }
   ] satisfies Milestone[],
 

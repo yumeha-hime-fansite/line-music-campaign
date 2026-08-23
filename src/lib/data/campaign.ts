87e8fb96e2381211b9b6a7646fb0c007d7a63609
yumeha-hime-fansite <yumeha.hime.fan.calendar@gmail.com>
2026-08-23 21:11:48 +0900

    Add files via upload

--- src/lib/data/campaign.ts
+++ src/lib/data/campaign.ts
@@ -15,38 +15,23 @@ export const campaign = {
   songTitle: "mal d'amour",
   campaignPeriod: '2026年9月30日まで',
 
-  // 次回キャンペーン時は、原則ここを差し替えればOKです。
-  // LINE MUSIC上の対象曲そのもののURL
+  // 計算機などで使うための機械可読データ
+  campaignEndDate: '2026-09-30',
+  songDurationSeconds: 181,
+
   songUrl: 'https://lin.ee/UrIsXn0',
-  // 今回の再生キャンペーン公式告知ページ
   officialCampaignUrl: 'https://x.com/hime_yumeha/status/2090438524176851336?s=20',
-  // 特典応募フォームのURL
   applicationUrl: 'https://docs.google.com/forms/d/e/1FAIpQLScS5L5WI8LA5uWGCdiUnAlvffLf2WMAJXJfIKU3-uhq2ly1tg/viewform?usp=send_form',
-  // 応募締切日
   applicationDeadline: '2026年10月3日',
 
-  // ヒメさんから掲載許可が出た場合に static/images 配下へ置き、
-  // '/images/xxx.webp' のように指定してください。未設定なら非表示になります。
-  individualCampaignImage: '',
-  totalCampaignImage: '',
+  individualCampaignImage: '/images/linemusic_campagin.jpeg',
+  totalCampaignImage: '/images/linemusic_campagin_target.jpeg',
 
   individualRewards: [
-    {
-      plays: 500,
-      title: 'スマホ用壁紙'
-    },
-    {
-      plays: 1_000,
-      title: 'ジャケットデザインステッカー'
-    },
-    {
-      plays: 5_000,
-      title: 'ジャケットデザイン缶バッジ'
-    },
-    {
-      plays: 9_999,
-      title: 'コンプリート記念カード'
-    }
+    { plays: 500, title: 'スマホ用壁紙' },
+    { plays: 1_000, title: 'ジャケットデザインステッカー' },
+    { plays: 5_000, title: 'ジャケットデザイン缶バッジ' },
+    { plays: 9_999, title: 'コンプリート記念カード' }
   ] satisfies Reward[],
 
   totalMilestones: [
@@ -67,10 +52,5 @@ export const campaign = {
     }
   ] satisfies Milestone[],
 
-  prePlayNotice: '',
-  playbackNotice: '',
-  applicationNotice: '',
-
-  // OGP画像を使う場合は /images/ogp.webp などを指定
-  ogImage: ''
+  ogImage: '/images/linemusic_campagin.jpeg'
 };
